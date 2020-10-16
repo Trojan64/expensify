524cc4ff8845a2e88ed4f6f750dabd84840e524a
viktorx64 <viktor.sundqvist2@gmail.com>
2020-10-16 14:23:18 +0200

    hotfix removed undefined

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@ fetch(`https://api.airtable.com/v0/appWJAgaz2kO3rrb9/Table%201?api_key=keyB1Op1R
     .then(data => {
         const { records } = data;
         const li = document.createElement("li");
-        var markup;
+        var markup = "";
 
         for(var i = 0; i < data.records.length; i++) {
         console.log(data.records[i].fields.Name + data.records[i].fields.spec + data.records[i].fields.Kr);

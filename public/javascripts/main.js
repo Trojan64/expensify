5932538a2178111c294f7ed16061057ab68f911b
viktorx64 <viktor.sundqvist2@gmail.com>
2020-10-16 14:04:25 +0200

    prining out data in html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,26 @@
 fetch(`https://api.airtable.com/v0/appWJAgaz2kO3rrb9/Table%201?api_key=keyB1Op1RxoiSdZi3`)
     .then(response => response.json())
     .then(data => {
-        const { records, fields, Name, Kr, spec } = data;
+        const { records } = data;
+        const li = document.createElement("li");
+        var markup = "";
+
         for(var i = 0; i < data.records.length; i++) {
         console.log(data.records[i].fields.Name + data.records[i].fields.spec + data.records[i].fields.Kr);
+
+        li.classList.add("expense");
+
+        markup += `
+                <div>
+                    <h2>
+                        <span>${data.records[i].fields.spec}</span>
+                        <span>${data.records[i].fields.Name}</span>
+                        <span>${data.records[i].fields.Kr} SEK</span>
+                    </h2>
+                </div>
+            `;
+
+        li.innerHTML = markup;
+        document.getElementById("expenses").appendChild(li);
         }
     })

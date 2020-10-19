babebdaec73b5332e07376384c7fd1f4ffdd4613
viktorx64 <viktor.sundqvist2@gmail.com>
2020-10-19 15:59:55 +0200

    added uploading data to server

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,19 +1,27 @@
 //add a element to the list
-fetch(`https://api.airtable.com/v0/appWJAgaz2kO3rrb9/Table%201?api_key=keyB1Op1RxoiSdZi3`, {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        "records": [
-              {
-                  "fields": {
-                      "Name": "detta är ett test",
-                      "Kr": 420,
-                      "spec": "med ch"
+function addData() {
+    //getting data from form
+    var component = document.getElementById("NewComponent").value;
+    var specs = document.getElementById("NewSpec").value;
+    var price = document.getElementById("NewPrice").value;
+    //send to server
+    fetch(`https://api.airtable.com/v0/appWJAgaz2kO3rrb9/Table%201?api_key=keyB1Op1RxoiSdZi3`, {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "records": [
+                  {
+                      "fields": {
+                          "Name": component,
+                          "Kr": parseInt(price),
+                          "spec": specs
+                      }
                   }
-              }
-          ]
-      })
-    })
-    .then(response => response.json())
+              ]
+          })
+        })
+        .then(response => response.json())
+    loadData();
+}
